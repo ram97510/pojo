78f291b0d7203855b6d90911be683a76e7678c4b
Ramkumar M <99495525+ram97510@users.noreply.github.com>
2024-10-05 10:32:30 +0530

    Automated update 2024-10-05T10:32:29.015

---
+++ c/main/java/com/example/mlapi/MlapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlapiApplication.class, args);
+	}
+
+}
