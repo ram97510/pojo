ef1ca9925df833b077abfb99a6efc37ae0676750
Ramkumar M <99495525+ram97510@users.noreply.github.com>
2024-10-08 17:50:55 +0530

    Automated update 2024-10-08T17:50:54.064

---
+++ i-pojo/src/test/java/com/example/mlapi/MlapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
